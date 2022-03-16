c2cbbdd57d8fbdae67939cc9c38f8ae90ab116d9
Tilde Bastos <tilde.bastos@gmail.com>
2022-03-15 21:56:14 -0300

    Remoção do método GetProductCategory da classe ProductService, substituindo por GetById

--- CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
+++ CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -22,7 +22,9 @@ namespace CleanArchMvc.Infra.Data.Repositories {
 
         public async Task<Product> GetByIdAsync(int? id) {
 
-            return await _productContext.Products.FindAsync(id);
+            //return await _productContext.Products.FindAsync(id);
+            return await _productContext.Products.Include(c => c.Category)
+                .SingleOrDefaultAsync(p => p.Id == id);
         }
 
         public async Task<IEnumerable<Product>> GetProductsAsync() {
@@ -30,11 +32,11 @@ namespace CleanArchMvc.Infra.Data.Repositories {
             return await _productContext.Products.ToListAsync();
         }
 
-        public async Task<Product> GetProductCategoryAsync(int? id) {
-            // eager loading
-            return await _productContext.Products.Include(c => c.Category)
-                .SingleOrDefaultAsync(p => p.Id == id);
-        }
+        //public async Task<Product> GetProductCategoryAsync(int? id) {
+        //    // eager loading
+        //    return await _productContext.Products.Include(c => c.Category)
+        //        .SingleOrDefaultAsync(p => p.Id == id);
+        //}
 
         public async Task<Product> RemoveAsync(Product product) {
 
